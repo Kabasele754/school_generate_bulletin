f429f215ee4c690e197622bffa78a5a3bede0e70
Achille kabasele <pepexykabasele@gmail.com>
2022-12-29 19:27:14 +0100

    mailbox template commit

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -6,4 +6,9 @@ path('student/',StudentView.as_view(), name="student"),
 path('staf/',StafView.as_view(), name="staf"),
 path('class/',ClassView.as_view(), name="class"),
 path('book/',BookView.as_view(), name="book"),
+
+# inbox
+
+   path('inbox/',InboxView.as_view(), name="inbox"),
+path('inbox/compose/',ComposeView.as_view(), name="compose"),
 ]
