d9c86f0d286ab27ce7f3dd4999d1b2016f648cf3
Achille kabasele <pepexykabasele@gmail.com>
2022-12-30 15:52:59 +0100

    update

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from  .views import *
 urlpatterns = [
    path('',DashbordardView.as_view(), name="dashboard_admin"),
-path('student/',StudentView.as_view(), name="student"),
-path('staf/',StafView.as_view(), name="staf"),
-path('class/',ClassView.as_view(), name="class"),
-path('book/',BookView.as_view(), name="book"),
+   path('student/',StudentView.as_view(), name="student"),
+   path('staf/',StafView.as_view(), name="staf"),
+   path('class/',ClassView.as_view(), name="class"),
+   path('book/',BookView.as_view(), name="book"),
 
-# inbox
+   # inbox
 
    path('inbox/',InboxView.as_view(), name="inbox"),
-path('inbox/compose/',ComposeView.as_view(), name="compose"),
+   path('inbox/compose/',ComposeView.as_view(), name="compose"),
+   path('bulletin/',BulletinView.as_view(), name="bulletin"),
 ]
