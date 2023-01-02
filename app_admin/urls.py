83f928a97edf1a42a188b59efa0b482ef7d50618
Achille kabasele <pepexykabasele@gmail.com>
2023-01-03 00:12:03 +0100

    update

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -3,8 +3,12 @@ from .views import *
 
 urlpatterns = [
    path('',admin_home, name="admin_home"),
+   # student
    path('student/',StudentView.as_view(), name="student"),
-   path('staf/',StaffView.as_view(), name="staff"),
+   # staff
+   path('staff/',StaffView.as_view(), name="staff"),
+   path('staff/affecter',AffecterCoursView.as_view(), name="affecter"),
+   # class
    path('class/',ClassView.as_view(), name="school_class"),
    path('course/',CourseView.as_view(), name="book"),
 
