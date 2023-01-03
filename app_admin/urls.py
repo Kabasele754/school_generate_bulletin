38ebb244d80522e3902e8826acb3bc889beb7c1a
Achille kabasele <pepexykabasele@gmail.com>
2023-01-03 20:07:31 +0100

    update

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -21,8 +21,17 @@ urlpatterns = [
    path("course/edit/<int:course_id>",
          edit_course, name='edit_course'),
 
+    # Url Article Blog and Category
+    path('add-category', AddCategoryView.as_view(),name="add_category"),
+    path('add-blog', AddBlogView.as_view(), name="add_blog"),
+    path('list-blog', AllBlogs.as_view(), name="list_blog"),
+    path('update/<int:article_id>', update_article, name="update"),
+    #path('delete/<int:book_id>', views.delete_book)
+
    # inbox
    path('inbox/',InboxView.as_view(), name="inbox"),
    path('inbox/compose/',ComposeView.as_view(), name="compose"),
    path('bulletin/',BulletinView.as_view(), name="bulletin"),
+
+
 ]
